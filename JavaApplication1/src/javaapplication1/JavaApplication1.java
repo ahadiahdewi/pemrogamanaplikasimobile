343e41aace4b9d3c3fe464c2371b4047e1836679
ahadiahdewi <43110726+ahadiahdewi@users.noreply.github.com>
2018-09-09 18:38:04 +0700

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author win 8.1
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int jumlah = 5;
+        // TODO code application logic here
+        for (int i=0;i<jumlah;i++){
+            for(int j=0;j<=i;j++){
+            System.out.print("*");
+            }
+            System.out.print("\n");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+
